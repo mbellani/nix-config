5a88d9d50e4ea23e65985e4fa13d322a4b3fee81
Manish Bellani <manish.bellani@gmail.com>
2026-01-08 14:47:40 -0600

    some config fixes

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -5,8 +5,8 @@
     enable = true;
     package = if pkgs.stdenv.isDarwin then pkgs.ghostty-bin else pkgs.ghostty;
     settings = {
-      macos-titlebar-style = hidden;
       font-family = "FiraCode Nerd Font";
+
       font-size = 14;
       theme = "dark";
       window-padding-x = 2;
